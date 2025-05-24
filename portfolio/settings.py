7a8a860cbffee23f61b1c261d311792940f9d9c7
Abhonotik <abhonotik14@gmail.com>
2025-05-24 15:10:16 +0530

    Fixed bug in user login and updated UI

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -126,10 +126,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# Add this in production:
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# During development
+STATICFILES_DIRS = [BASE_DIR / 'static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
